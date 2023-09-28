a1e13edd43d8d56d9ea2258288dc1f1930120964
ManoaNyavo04 <120180988+ManoaNyavo04@users.noreply.github.com>
2023-09-28 16:43:48 +0100

    sprint

---
+++ assets/pages/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <table border="1">
+            <tr>
+                <th>
+                    <td>Pays</td>
+                    <td>Capital</td>
+                </th>
+            </tr>
+
+            <tbody>
+                <tr>
+                    <td>Madagascar</td>
+                    <td>Antananarivo</td>
+                </tr>
+                
+            </tbody>
+        </table>
+        
+    </div>
+</body>
+</html>
